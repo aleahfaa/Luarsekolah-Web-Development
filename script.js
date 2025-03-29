b2769dbed467eb867d8e32ca2d902eedf40d5ce3
aleahfaa <iffaamaliasabrina@gmail.com>
2025-03-29 11:30:18 +0700

    Tugas 3: JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const heroSection = document.querySelector('#home');
 const images = [
-    './hero-section.jpg',
-    './hero-section-2.jpg',
-    './hero-section-3.jpg',
-    './hero-section-4.jpg',
-    './hero-section-5.jpg',
-    './hero-section-6.jpg'
+    './img/hero-section.jpg',
+    './img/hero-section-2.jpg',
+    './img/hero-section-3.jpg',
+    './img/hero-section-4.jpg',
+    './img/hero-section-5.jpg',
+    './img/hero-section-6.jpg'
 
 ];
 let currentIndex = 0;
@@ -14,3 +14,34 @@ function changeBackgroundImage() {
     heroSection.style.backgroundImage = `url(${images[currentIndex]})`;
 }
 setInterval(changeBackgroundImage, 3000);
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+});
